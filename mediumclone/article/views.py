d3fd80d4980a2f3743b07862c4fed7c9758ae124
forestbaba <hawlahethan@gmail.com>
2019-12-13 16:26:03 +0100

    First commit

---
+++ mediumclone/article/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
+        #return Response({"articles": articles})
